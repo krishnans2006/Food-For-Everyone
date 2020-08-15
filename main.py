0305def9ff91e26444bc1db198e9a939eb8837c8
KrishnanS2006 <62958782+KrishnanS2006@users.noreply.github.com>
2020-08-14 23:30:10 -0400

    Basic skeleton flask app

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+# Needs 2 buttons - Sign Up and Log In
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+# @app.route("/signup")
+# def sign_up():
+#     return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
